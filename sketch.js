aee8801beb9f9a5510d1c364310e348f33c8dbbd
lorenzobarilla <37076142+lorenzobarilla@users.noreply.github.com>
2021-05-30 22:44:34 +0200

    php - save page test

--- sketch.js
+++ sketch.js
@@ -224,3 +224,43 @@ function createLegenda() {
     ul.appendChild(li);
   }
 }
+
+function saveExercise() {
+  var myExercise = document.querySelector('#parContainer');
+
+  var ctx = myExercise.getContext('2d');
+
+
+  myExercise.save('myExercise', 'png');
+}
+
+function doCapture() {
+    window.scrollTo(0, 0);
+
+    html2canvas(document.getElementById("parContainer")).then(function (canvas) {
+
+        // Create an AJAX object
+        var ajax = new XMLHttpRequest();
+
+        // Setting method, server file name, and asynchronous
+        ajax.open("POST", "save-capture.php", true);
+
+        // Setting headers for POST method
+        ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+        // Sending image data to server
+        ajax.send("image=" + canvas.toDataURL("image/jpeg", 0.9));
+
+        // Receiving response from server
+        // This function will be called multiple times
+        ajax.onreadystatechange = function () {
+
+            // Check when the requested is completed
+            if (this.readyState == 4 && this.status == 200) {
+
+                // Displaying response from server
+                console.log(this.responseText);
+            }
+        };
+    });
+}
